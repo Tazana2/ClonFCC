b5bd9bc3440c57cf3300167429419264201d5b56
Tazana2 <danisanmez@gmail.com>
2022-06-06 19:58:37 -0500

    Cambios al README y se añaden creditos generales

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <div className='contenedor-principal'>
-        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <h1>Esto es lo que dicen los alumnos de freeCodeCamp:</h1>
         <Testimonio 
                 nombre="Shawn Wang"
                 pais="Singapur"
@@ -29,7 +29,7 @@ function App() {
         cargo="Ingeniera de Software"
         empresa="Spotify"
         testimonio="Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."/>
-
+        <p>Sitio Web creado gracias a este <a href='https://youtu.be/6Jfk8ic3KVk' target={"_blank"} rel="noreferrer">video</a>.</p>
       </div>
     </div>
   );
